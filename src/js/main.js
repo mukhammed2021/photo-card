8abfa3456407094d5a58ea4e31f879fc6ce056c3
Mukhammed <massalimov.mukhammed@mail.ru>
2024-07-31 22:39:30 +0500

    Header top

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,12 @@
 import "./swiper";
 import "../style/style.scss";
+
+document.addEventListener("click", documentActions);
+
+function documentActions(e) {
+   const targetElement = e.target;
+
+   if (targetElement.closest(".icon-menu")) {
+      document.documentElement.classList.toggle("menu-open");
+   }
+}
