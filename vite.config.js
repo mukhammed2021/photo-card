3ec2ca027e99b6d829e09a5d83244337d2650ba4
Mukhammed <massalimov.mukhammed@mail.ru>
2024-08-02 15:33:46 +0500

    Vacancies page

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ const SVG_FOLDER_PATH = path.resolve(SRC_PATH, "svg-sprite");
 
 export default defineConfig({
    plugins: [injectHTML(), createSvgSpritePlugin({ svgFolder: SVG_FOLDER_PATH })],
+   build: {
+      rollupOptions: {
+         input: {
+            main: path.resolve(__dirname, "index.html"),
+            vacancies: path.resolve(__dirname, "src/html/pages/vacancies/index.html"),
+         },
+      },
+   },
 });
